fbbdd8be3d298a75ebd50120e16d71da4579a2a8
Vilvarin <viilvariin@gmail.com>
2015-11-06 03:16:06 +0500

    Вёрстка

---
+++ js/filling_field.js
@@ -0,0 +1,8 @@
+/*$(function(){
+	for(var x = 0; i < 450; i += 50){
+		for(var y = 0; i < 450; i += 50){
+			$('<div class="tile"><div class="shape"></div></div>')
+				.appendTo('.game-field');
+		}
+	}
+});
